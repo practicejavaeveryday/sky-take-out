73e4036aa0b46a116bdcf6b7a5792a41a8c1b42b
practicejavaeveryday <wangtianyue106@163.com>
2024-11-07 14:12:20 +0800

    员工启用禁用

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -89,4 +89,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(total,records);
     }
 
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Employee employee = new Employee();
+        employee.setStatus(status);
+        employee.setId(id);
+        employeeMapper.update(employee);
+    }
+
 }
